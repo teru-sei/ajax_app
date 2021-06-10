af1ad0ad5e9636f37382eaa48cf51aaf6cb89514
teru-sei <hfc.3591@gmail.com>
2021-06-10 16:40:26 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.order(id: "DESC")
+    @posts = Post.all
   end
 
+<<<<<<< HEAD
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
+=======
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+>>>>>>> parent of d016830 (AjaxAppの準備)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root to: 'posts#index'
+=======
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+>>>>>>> parent of d016830 (AjaxAppの準備)
   post 'posts', to: 'posts#create'
 end
