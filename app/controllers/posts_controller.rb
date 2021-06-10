8b0572f899584c04ecfb6099454e15239ff1fbfd
teru-sei <hfc.3591@gmail.com>
2021-06-10 14:30:51 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
+  
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
