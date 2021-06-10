6288a6d26543da613af5640d1f7b1b38a4b7c7d9
teru-sei <hfc.3591@gmail.com>
2021-06-10 11:25:28 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> parent of 9c4978f (JSでレスポンスを処理する)
   def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
-=======
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
->>>>>>> parent of d016830 (AjaxAppの準備)
-=======
- # def new
- # end
-
-  def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
->>>>>>> parent of 10f3773 (リクエストを処理する)
   end
 end
